f40a2b790d8ce99481cc3c29bf5374b400c3bf6c
scottms724 <scottms724@gmail.com>
2021-06-28 11:19:41 -0400

    add dynamic month urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("It works!") 
+monthly_challenges = {
+    "january": "The current month is January.",
+    "february": "The current month is February.",
+    "march": "The current month is March.",
+    "april": "The current month is April.",
+    "may": "The current month is May.",
+    "june": "The current month is June.",
+    "july": "The current month is July.",
+    "august": "The current month is August.", 
+    "september": "The current month is September.", 
+    "october": "The current month is October.",
+    "november": "The current month is November.",
+    "december": "The current month is December."
+}
+
+def monthly_challenge_by_number(request, month_integer):
+    months = list(monthly_challenges.keys())
+
+    if month_integer > len(months):
+        return HttpResponseNotFound("Please enter a valid month.")
+
+    redirect_month = months[month_integer - 1]
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except: 
+        return HttpResponseNotFound("Please enter a valid month.")
+    return HttpResponse(challenge_text)
