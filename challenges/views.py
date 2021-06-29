00acd8942d3765de0a4b4a444d705d32ce240aa1
scottms724 <scottms724@gmail.com>
2021-06-29 12:54:17 -0400

    add title filters and for and if tags

--- challenges/views.py
+++ challenges/views.py
@@ -17,20 +17,15 @@ monthly_challenges = {
     "september": "The current month is September.", 
     "october": "The current month is October.",
     "november": "The current month is November.",
-    "december": "The current month is December."
+    "december": None 
 }
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months: 
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months 
+    })
 
 
 
@@ -51,7 +46,7 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
         return render(request, "challenges/challenge.html", {
             "text": challenge_text,
-            "month_name": month.capitalize()
+            "month_name": month
         })
     except: 
         return HttpResponseNotFound("<h1>Please enter a valid month.</h1>")
